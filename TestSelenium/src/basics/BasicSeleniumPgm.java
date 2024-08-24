458308fbad94b0223f7d4dd23401100ef877ee7d
jijin <jijin@Sneha-jijin>
2024-08-24 09:15:13 +0530

    Selenium Basic codes

---
+++ TestSelenium/src/basics/BasicSeleniumPgm.java
@@ -0,0 +1,32 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicSeleniumPgm {
+
+	public static void main(String[] args) {
+		//Bringing driver
+		WebDriver driver=new ChromeDriver();
+		
+		//Maximize the browser window
+		driver.manage().window().maximize();
+		
+		//Open the browser
+		driver.get("https://www.facebook.com");
+		
+		//Get the title of the page
+		String tiltle=driver.getTitle();
+		System.out.println(tiltle);
+		
+		//get the current opened URL
+		String URL=driver.getCurrentUrl();
+		System.out.println(URL);
+		
+		//Closing the browser
+		driver.close();
+		
+
+	}
+
+}
